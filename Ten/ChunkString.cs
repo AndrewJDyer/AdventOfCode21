287abed2e132af7c6e1af13e0a833c3bfd427f17
Andrew <andrew@dyers.me.uk>
2021-12-27 00:10:30 +0000

    Day 10a

---
+++ Ten/ChunkString.cs
@@ -0,0 +1,69 @@
+﻿namespace Ten;
+
+internal class ChunkString
+{
+    private readonly string text;
+    private readonly Stack<char> openingBrackets = new();
+    private static readonly Dictionary<char, char> bracketPairs = new()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' }
+    };
+
+    private int index;
+
+    public ChunkString(string text) => this.text = text;
+
+    public int CalculateErrorScore()
+    {
+        while (index < text.Length)
+        {
+            try
+            {
+                RegisterChar();
+            }
+            catch (InvalidCharException)
+            {
+                return ScoreChar();
+            }
+            index++;
+        }
+
+        return 0;
+    }
+
+    private bool IsOpeningBracket() => bracketPairs.ContainsKey(GetCurrentChar());
+
+    private char GetExpectedClosingBrace()
+    {
+        if (openingBrackets.Count == 0)
+            throw new InvalidCharException();
+
+        return bracketPairs[openingBrackets.Peek()];
+    }
+
+    private int ScoreChar() => GetCurrentChar() switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => throw new InvalidCharException()
+    };
+
+    private void RegisterChar()
+    {
+        if (IsOpeningBracket())
+            openingBrackets.Push(GetCurrentChar());
+        else if (GetCurrentChar() == GetExpectedClosingBrace())
+            openingBrackets.Pop();
+        else
+            throw new InvalidCharException();
+    }
+
+    private char GetCurrentChar() => text[index];
+
+    private class InvalidCharException : Exception { }
+}
---
+++ Ten/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ten;
+
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadAllLines(@"C:\Users\hapdy\OneDrive\Andy\Code\AdventOfCode21\Ten\input.txt");
+var scoreSum = lines.Sum(l => new ChunkString(l).CalculateErrorScore());
+
+Console.WriteLine(scoreSum);
