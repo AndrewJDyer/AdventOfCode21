61b159273f45e770bdc95df9c8a979ddb60428c9
Andrew <andrew@dyers.me.uk>
2021-12-27 22:16:14 +0000

    Day 11b

--- Eleven/Program.cs
+++ Eleven/Program.cs
@@ -2,8 +2,14 @@
 using Eleven;
 
 var parser = new Parser(@"C:\Users\hapdy\OneDrive\Andy\Code\AdventOfCode21\Eleven\input.txt");
-var cavern = new Cavern(parser.Parse());
-var counter = new FlashCounter(cavern, 100);
-var count = counter.Count();
+var initialEnergies = parser.Parse();
+var counter = new FlashCounter(new (initialEnergies), 100);
+var part1Answer = counter.Count();
 
-Console.WriteLine(count);
+Console.WriteLine(part1Answer);
+
+var syncNotifier = new SyncNotifier(new(initialEnergies));
+var part2Answer = syncNotifier.GetRoundOfFirstSync();
+
+Console.WriteLine(part2Answer);
+
---
+++ Eleven/SyncNotifier.cs
@@ -0,0 +1,48 @@
+﻿namespace Eleven;
+
+internal class SyncNotifier
+{
+    private readonly Cavern cavern;
+
+    public SyncNotifier(Cavern cavern)
+    {
+        this.cavern = cavern;
+        foreach (var octopus in cavern.Octopi)
+            octopus.Initialise();
+    }
+
+    public int GetRoundOfFirstSync()
+    {
+        for (int roundCount = 1; true; roundCount++)
+        {
+            var roundRunner = new RoundRunner(cavern);
+            roundRunner.RunRound();
+            if (roundRunner.OctopiAreSynchronised)
+                return roundCount;
+        }
+    }
+
+    private class RoundRunner
+    {
+        private readonly Cavern cavern;
+        private int flashCount;
+
+        public bool OctopiAreSynchronised => flashCount == cavern.Octopi.Count();
+
+        public RoundRunner(Cavern cavern)
+        {
+            this.cavern = cavern;
+            foreach (var octopus in cavern.Octopi)
+                octopus.Flash += (_, _) => flashCount++;
+        }
+
+        public void RunRound()
+        {
+            foreach (var octopus in cavern.Octopi)
+                octopus.ResetRound();
+
+            foreach (var octopus in cavern.Octopi)
+                octopus.Step();
+        }
+    }
+}
